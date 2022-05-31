f2788244ee76ed088e2e4d74ed29779d196db164
Maarten de Vries <maarten@de-vri.es>
2022-05-31 11:47:56 +0200

    Report angles in order that they should be applied.

--- src/tests.rs
+++ src/tests.rs
@@ -24,28 +24,28 @@ fn test_convert_axis_angle_to_euler() {
 	assert_relative_eq!(euler_ry, 0.2035335, epsilon = 1e-6);
 	assert_relative_eq!(euler_rz, 0.1479187, epsilon = 1e-6);
 
-	let (euler_rx, euler_ry, euler_rz) = convert_axis_angle_to_euler_yxz(rx, ry, rz, angle);
-	assert_relative_eq!(euler_rx, 0.0775457, epsilon = 1e-6);
+	let (euler_ry, euler_rx, euler_rz) = convert_axis_angle_to_euler_yxz(rx, ry, rz, angle);
 	assert_relative_eq!(euler_ry, 0.2041556, epsilon = 1e-6);
+	assert_relative_eq!(euler_rx, 0.0775457, epsilon = 1e-6);
 	assert_relative_eq!(euler_rz, 0.1639563, epsilon = 1e-6);
 
-	let (euler_rx, euler_ry, euler_rz) = convert_axis_angle_to_euler_zxy(rx, ry, rz, angle);
+	let (euler_rz, euler_rx, euler_ry) = convert_axis_angle_to_euler_zxy(rx, ry, rz, angle);
+	assert_relative_eq!(euler_rz, 0.1457008, epsilon = 1e-6);
 	assert_relative_eq!(euler_rx, 0.1081443, epsilon = 1e-6);
 	assert_relative_eq!(euler_ry, 0.1898811, epsilon = 1e-6);
-	assert_relative_eq!(euler_rz, 0.1457008, epsilon = 1e-6);
 
-	let (euler_rx, euler_ry, euler_rz) = convert_axis_angle_to_euler_zyx(rx, ry, rz, angle);
-	assert_relative_eq!(euler_rx, 0.1101078, epsilon = 1e-6);
-	assert_relative_eq!(euler_ry, 0.1887585, epsilon = 1e-6);
+	let (euler_rz, euler_ry, euler_rx) = convert_axis_angle_to_euler_zyx(rx, ry, rz, angle);
 	assert_relative_eq!(euler_rz, 0.1664424, epsilon = 1e-6);
+	assert_relative_eq!(euler_ry, 0.1887585, epsilon = 1e-6);
+	assert_relative_eq!(euler_rx, 0.1101078, epsilon = 1e-6);
 
-	let (euler_rx, euler_ry, euler_rz) = convert_axis_angle_to_euler_yzx(rx, ry, rz, angle);
-	assert_relative_eq!(euler_rx, 0.0785954, epsilon = 1e-6);
+	let (euler_ry, euler_rz, euler_rx) = convert_axis_angle_to_euler_yzx(rx, ry, rz, angle);
 	assert_relative_eq!(euler_ry, 0.1913399, epsilon = 1e-6);
 	assert_relative_eq!(euler_rz, 0.1634591, epsilon = 1e-6);
+	assert_relative_eq!(euler_rx, 0.0785954, epsilon = 1e-6);
 
-	let (euler_rx, euler_ry, euler_rz) = convert_axis_angle_to_euler_xzy(rx, ry, rz, angle);
+	let (euler_rx, euler_rz, euler_ry) = convert_axis_angle_to_euler_xzy(rx, ry, rz, angle);
 	assert_relative_eq!(euler_rx, 0.1092933, epsilon = 1e-6);
-	assert_relative_eq!(euler_ry, 0.2057181, epsilon = 1e-6);
 	assert_relative_eq!(euler_rz, 0.1448436, epsilon = 1e-6);
+	assert_relative_eq!(euler_ry, 0.2057181, epsilon = 1e-6);
 }
