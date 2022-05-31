6859c8eb2f97202ec6cc3e4357ee6abc05028235
Maarten de Vries <maarten@de-vri.es>
2022-05-31 11:48:31 +0200

    Add random tests to verify implementation against nalgebra.

--- src/tests.rs
+++ src/tests.rs
@@ -1,15 +1,17 @@
+use approx::assert_relative_eq;
+use nalgebra as na;
+
+use crate::{
+	convert_axis_angle_to_euler_xyz,
+	convert_axis_angle_to_euler_yxz,
+	convert_axis_angle_to_euler_zxy,
+	convert_axis_angle_to_euler_zyx,
+	convert_axis_angle_to_euler_yzx,
+	convert_axis_angle_to_euler_xzy
+};
+
 #[test]
 fn test_convert_axis_angle_to_euler() {
-	use approx::assert_relative_eq;
-
-	use crate::{
-		convert_axis_angle_to_euler_xyz,
-		convert_axis_angle_to_euler_yxz,
-		convert_axis_angle_to_euler_zxy,
-		convert_axis_angle_to_euler_zyx,
-		convert_axis_angle_to_euler_yzx,
-		convert_axis_angle_to_euler_xzy
-	};
 
 	// Using random values (vector is normalized).
 	let rx = 0.35000955;
@@ -48,4 +50,108 @@ fn test_convert_axis_angle_to_euler() {
 	assert_relative_eq!(euler_rx, 0.1092933, epsilon = 1e-6);
 	assert_relative_eq!(euler_rz, 0.1448436, epsilon = 1e-6);
 	assert_relative_eq!(euler_ry, 0.2057181, epsilon = 1e-6);
+}
+
+fn rand(random: &mut impl rand::Rng, min: f64, max: f64) -> f64 {
+	use rand::distributions::Distribution;
+
+	let dist = rand::distributions::Uniform::new(min, max);
+	dist.sample(random)
+}
+
+fn rotation(axis_x: f64, axis_y: f64, axis_z: f64, angle: f64) -> na::UnitQuaternion<f64> {
+	let axis = na::Vector3::new(axis_x, axis_y, axis_z);
+	let axis = na::UnitVector3::new_normalize(axis);
+	na::UnitQuaternion::from_axis_angle(&axis, angle)
+}
+
+fn rot_x(angle: f64) -> na::UnitQuaternion<f64> {
+	rotation(1.0, 0.0, 0.0, angle)
+}
+
+fn rot_y(angle: f64) -> na::UnitQuaternion<f64> {
+	rotation(0.0, 1.0, 0.0, angle)
+}
+
+fn rot_z(angle: f64) -> na::UnitQuaternion<f64> {
+	rotation(0.0, 0.0, 1.0, angle)
+}
+
+macro_rules! assert_vec {
+	($vec_a:expr, $vec_b: expr, $epsilon:expr) => {
+		{
+			let vec_a: nalgebra::Vector<_, _, _> = $vec_a;
+			let vec_b: nalgebra::Vector<_, _, _> = $vec_b;
+			let distance = (vec_a - vec_b).norm();
+			let epsilon = $epsilon;
+			if distance > epsilon {
+				::assert2::assert!($vec_a == $vec_b, "distance ({distance}) > epsilon ({epsilon})")
+			}
+		}
+	}
+}
+
+fn random_samples() -> impl Iterator<Item = (f64, f64, f64, f64)> {
+	let mut random: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(0);
+	(0..1_000).into_iter().map(move |_| {
+		let axis_x = rand(&mut random, 0.0, 1.0);
+		let axis_y = rand(&mut random, 0.0, 1.0);
+		let axis_z = rand(&mut random, 0.0, 1.0);
+		let angle = rand(&mut random, 0.0, std::f64::consts::TAU);
+		(axis_x, axis_y, axis_z, angle)
+	})
+}
+
+#[test]
+fn test_random_xyz() {
+	for (axis_x, axis_y, axis_z, angle) in random_samples() {
+		let rotation = rotation(axis_x, axis_y, axis_z, angle);
+		let (rx, ry, rz) = convert_axis_angle_to_euler_xyz(axis_x, axis_y, axis_z, angle);
+		assert_vec!((rot_x(rx) * rot_y(ry) * rot_z(rz)).scaled_axis(), rotation.scaled_axis(), 1e-10);
+	}
+}
+
+#[test]
+fn test_random_yxz() {
+	for (axis_x, axis_y, axis_z, angle) in random_samples() {
+		let rotation = rotation(axis_x, axis_y, axis_z, angle);
+		let (ry, rx, rz) = convert_axis_angle_to_euler_yxz(axis_x, axis_y, axis_z, angle);
+		assert_vec!((rot_y(ry) * rot_x(rx) * rot_z(rz)).scaled_axis(), rotation.scaled_axis(), 1e-10);
+	}
+}
+
+#[test]
+fn test_random_zxy() {
+	for (axis_x, axis_y, axis_z, angle) in random_samples() {
+		let rotation = rotation(axis_x, axis_y, axis_z, angle);
+		let (rz, rx, ry) = convert_axis_angle_to_euler_zxy(axis_x, axis_y, axis_z, angle);
+		assert_vec!((rot_z(rz) * rot_x(rx) * rot_y(ry)).scaled_axis(), rotation.scaled_axis(), 1e-10);
+	}
+}
+
+#[test]
+fn test_random_zyx() {
+	for (axis_x, axis_y, axis_z, angle) in random_samples() {
+		let rotation = rotation(axis_x, axis_y, axis_z, angle);
+		let (rz, ry, rx) = convert_axis_angle_to_euler_zyx(axis_x, axis_y, axis_z, angle);
+		assert_vec!((rot_z(rz) * rot_y(ry) * rot_x(rx)).scaled_axis(), rotation.scaled_axis(), 1e-10);
+	}
+}
+
+#[test]
+fn test_random_yzx() {
+	for (axis_x, axis_y, axis_z, angle) in random_samples() {
+		let rotation = rotation(axis_x, axis_y, axis_z, angle);
+		let (ry, rz, rx) = convert_axis_angle_to_euler_yzx(axis_x, axis_y, axis_z, angle);
+		assert_vec!((rot_y(ry) * rot_z(rz) * rot_x(rx)).scaled_axis(), rotation.scaled_axis(), 1e-10);
+	}
+}
+
+#[test]
+fn test_random_xzy() {
+	for (axis_x, axis_y, axis_z, angle) in random_samples() {
+		let rotation = rotation(axis_x, axis_y, axis_z, angle);
+		let (rx, rz, ry) = convert_axis_angle_to_euler_xzy(axis_x, axis_y, axis_z, angle);
+		assert_vec!((rot_x(rx) * rot_z(rz) * rot_y(ry)).scaled_axis(), rotation.scaled_axis(), 1e-10);
+	}
 }
